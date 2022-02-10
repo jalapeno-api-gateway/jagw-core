588bf200e0d5caad1ccd56d61a717cab4278acf2
Michel Bongard <michel_bongard@hotmail.com>
2022-02-10 18:07:03 +0100

    updated logger

---
+++ logger/logger.go
@@ -0,0 +1,40 @@
+package logging
+
+import (
+	"fmt"
+	"path"
+	"runtime"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Init(l *logrus.Logger, logLevel string) {
+	l.SetReportCaller(true)
+	l.SetFormatter(&logrus.JSONFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+	})
+	l.SetLevel(getLogrusLogLevel(logLevel))
+}
+
+func getLogrusLogLevel(logLevel string) logrus.Level {
+	switch strings.ToLower(logLevel) {
+	case "trace":
+		return logrus.TraceLevel
+	case "debug":
+		return logrus.DebugLevel
+	case "info":
+		return logrus.InfoLevel
+	case "error":
+		return logrus.ErrorLevel
+	case "fatal":
+		return logrus.FatalLevel
+	case "panic":
+		return logrus.PanicLevel
+	default:
+		return logrus.WarnLevel
+	}
+}
