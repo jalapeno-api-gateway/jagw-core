0ecbeec9206bcbfdf1f2887dcf09b2e8673c8777
mbongard <michel.bongard@ost.ch>
2022-02-11 11:55:17 +0100

    Added support for error handling

---
+++ error/error.go
@@ -0,0 +1,41 @@
+package error
+
+import (
+	"google.golang.org/grpc/codes"
+)
+
+type ErrorCode string
+
+const (
+	UNKNOWN           ErrorCode = "UNKNOWN"
+	INVALID_ARGUMENT  ErrorCode = "INVALID_ARGUMENT"
+	DEADLINE_EXCEEDED ErrorCode = "DEADLINE_EXCEEDED"
+	NOT_FOUND         ErrorCode = "NOT_FOUND"
+	UNIMPLEMENTED     ErrorCode = "UNIMPLEMENTED"
+	INTERNAL          ErrorCode = "INTERNAL"
+	UNAVAILABLE       ErrorCode = "UNAVAILABLE"
+)
+
+type Error struct {
+	ErrorCode ErrorCode
+	Message   string
+}
+
+func GetGrpcErrorCode(err Error) codes.Code {
+	switch err.ErrorCode {
+	case "INVALID_ARGUMENT":
+		return codes.InvalidArgument
+	case "DEADLINE_EXCEEDED":
+		return codes.DeadlineExceeded
+	case "NOT_FOUND":
+		return codes.NotFound
+	case "UNIMPLEMENTED":
+		return codes.Unimplemented
+	case "INTERNAL":
+		return codes.Internal
+	case "UNAVAILABLE":
+		return codes.Unavailable
+	default:
+		return codes.Unknown
+	}
+}
